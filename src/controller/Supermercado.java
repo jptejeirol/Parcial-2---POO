8f20f5a30588a58103a2d59377655a7de00ab80c
ESTUDIANTE <ESTUDIANTE@10.220.6.219>
2024-08-08 14:21:39 -0500

    Creación de packages y clases

---
+++ src/controller/Supermercado.java
@@ -0,0 +1,15 @@
+package controller;
+
+/**
+ *
+ * @author Juan Pablo Tejeiro Londoño
+ */
+public class Supermercado {
+
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
